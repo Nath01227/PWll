6fae567846fabe98aa708dfdd86efeae87a615f0
Nath01227 <nathaliasantosss.27@gmail.com>
2025-04-10 16:27:36 -0300

    Update functions07.php
    Listas

--- array_functions/functions07.php
+++ array_functions/functions07.php
@@ -32,8 +32,39 @@
     echo $pessoa ['nome'] .'voto opcional';
 
     echo '<br>';
+     
 }
+
+    prova 
    }
+<?php
+	$numeros = [5,-5,1,-22,25,-15,15,-30,25,-2];
+	$numMaior = 0;
+    $numMenor = 0;
+
+
+
+
+    foreach ($numeros as $key => $value) {
+        
+        if ($value > 0) {
+            $numMaior++;
+        } else if ($value < 0) {
+           $numMenor++;
+        }
+    };
+ 
+        echo "Existem ${numMaior} números maiores que 0 e ${numMenor} números menores que 0.";
+     
+    
+    
+
+    
+    
+	
+
+	
+?>
    
 
 ?>
