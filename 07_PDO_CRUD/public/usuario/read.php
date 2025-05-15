fdce68b806b6c9d298d073bd148f1af2035d7093
Nathy01227 <nathaliasantosss.27@gmail.com>
2025-05-15 18:21:52 -0300

    Aula 15/05

---
+++ 07_PDO_CRUD/public/usuario/read.php
@@ -0,0 +1,17 @@
+<?php
+    include '../../config/connection.php';
+
+
+    $stmt = $pdo->query('SELECT * FROM usuario');
+    $usuarios = $stmt->fetchAll();
+
+    // echo '<pre>';
+    // echo var_dump($usuarios);
+?>
+
+<?php foreach ($usuarios as $indice => $user) {?>
+    <p><strong>Nome: </strong><?php echo $user['username']; ?> </p>
+    <a href="http://localhost/PWLL/07_PDO_CRUD/public/usuario/delete.php?id=<?php echo $user['id']?>">Remover</a>
+    <a href="">Editar</a>
+    <hr>
+    <?php } ?>
